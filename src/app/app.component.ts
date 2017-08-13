ad19754aad1a000ac684e3a6ff0d349cf2362199
Alejandro Lora <alejandrofpo@gmail.com>
2017-08-13 20:58:06 +0100

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  link = 'https://join.slack.com/t/aprendiendo-android/shared_invite/MjI1OTQ1NTAzNjAzLTE1MDI2NTMwOTUtY2E4MjU5NmNhYQ';
+}
