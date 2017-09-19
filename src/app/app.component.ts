434f3c623bfbb8bd24d6df73e6a0cf78fc23ad34
Alejandro <alejandrofpo@gmail.com>
2017-09-19 20:01:58 +0100

    update link until 19 October

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  link = 'https://join.slack.com/t/aprendiendo-android/shared_invite/MjI1OTQ1NTAzNjAzLTE1MDI2NTMwOTUtY2E4MjU5NmNhYQ';
+  link = 'https://join.slack.com/t/aprendiendo-android/shared_invite/enQtMjQ0Njg2NjIwNjQ3LThlOTE2OTE1NTM5NGFmOWIyMzVkMjNlOWJjZDYzZTQxM2I3ODgzNTBjOWIwNGFhM2I3OTA2MzI2MWZjMzY5YzE';
 }
