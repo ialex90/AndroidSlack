d7a5f0c401250b8a2413353a0eb83c40541a264a
Alejandro <alejandrofpo@gmail.com>
2019-09-25 13:48:59 +0200

    Update link invitation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  link = 'https://join.slack.com/t/aprendiendo-android/shared_invite/enQtNTg2NDc5OTcyMDY3LTQ0YTRmNzM5MzRlYzU1ZTc0NjhiN2U0OTI5MDk2MzhjNDA1YjIxNGRmYjFhMDRlZDlmNGI2OTNiMTQ2ZTlmOTc';
+  link = 'https://join.slack.com/t/aprendiendo-android/shared_invite/enQtNTg2NDc5OTcyMDY3LWNmYzIyYmIwYmE4ZjQ4ZDA5OGZkNzg1YjA3ZmViOWM3NWFiZTAxMWYzMzRlZjhlN2EyODI5MWQwMmI4N2Y0ZjY';
 }
